364523642c8827b741c612cc80b77c565cfe26df
Yongdae Kim <yongdae91@hanmail.net>
2016-01-03 18:21:59 +0900

    Post 유효성 검사 테스트 추가

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create a new post' do
+    post = Post.new
+    post.title = '테스트 제목'
+    post.body = '테스트 내용'
+    assert post.save
+  end
+
+  test 'cannot create a new post without title' do
+    post = Post.new
+    post.body = '테스트 내용'
+    assert_not post.save
+  end
+
+  test 'cannot create a new post without body' do
+    post = Post.new
+    post.title = '테스트 제목'
+    assert_not post.save
+  end
 end
